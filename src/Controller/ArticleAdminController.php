85779670b77343f302a798a05965fb90539b1a85
Daniel Backes <danielbackes@protonmail.com>
2021-06-19 13:59:02 -0400

    feat: develop article edit action

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -41,12 +41,28 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article/{id}/edit")
+     * @Route("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, EntityManagerInterface $em, Request $request)
     {
-        dd($article);
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Article Updated! Inaccuracies Squashed!');
+
+            return $this->redirectToRoute('admin_article_edit', [
+                'id' => $article->getId(),
+            ]);
+        }
+
+        return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
     }
 
     /**
