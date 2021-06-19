f8d5114a15c4378f2fc1377e2fa0c0dafd178c5f
Daniel Backes <danielbackes@protonmail.com>
2021-06-19 16:34:34 -0400

    feat: add validation on register form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,20 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ *
+ * @UniqueEntity(
+ *  fields={
+ *    "email"
+ *  },
+ *  message="I think you've already registered!",
+ * )
  */
 class User implements UserInterface
 {
@@ -23,6 +32,10 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Groups("main")
+     * @Assert\NotBlank(
+     *  message="Please enter an email",
+     * )
+     * @Assert\Email()
      */
     private $email;
 
