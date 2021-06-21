5a8cc6f70970844859a4f3d22042ab742b38d062
Daniel Backes <danielbackes@protonmail.com>
2021-06-21 16:02:59 -0400

    feat: change form based on data

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -20,6 +20,9 @@ class ArticleFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $article = $options['data'] ?? null;
+        $isEdit = $article && $article->getId();
+
         $builder
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy!',
@@ -27,17 +30,23 @@ class ArticleFormType extends AbstractType
             ->add('content', null, [
                 'rows' => 25,
             ])
-            ->add('publishedAt', null, [
-                'widget' => 'single_text',
+            ->add('author', UserSelectTextType::class, [
+                'disabled' => $isEdit,
             ])
-            ->add('author', UserSelectTextType::class)
         ;
+
+        if ($options['include_published_at']) {
+            $builder->add('publishedAt', null, [
+                'widget' => 'single_text',
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
+            'include_published_at' => false,
         ]);
     }
 }
